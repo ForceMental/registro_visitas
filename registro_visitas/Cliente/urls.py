6c85517ad6c081c6b9341e0e40bd879adb1bc2ae
Shiryu Valecillos Personal <shiryuevfr@gmail.com>
2023-11-19 23:59:10 -0300

    Se corrige update cliente

--- registro_visitas/Cliente/urls.py
+++ registro_visitas/Cliente/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('clientes/', views.ClienteListView.as_view(), name='cliente-list'),
     path('clientes/create/', views.ClienteCreateView.as_view(), name='cliente-create'),
+    path('clientes/update/<int:pk>/', views.ClienteUpdateView.as_view(), name='cliente-detail'),
     path('clientes/<int:pk>/', views.ClienteDetailView.as_view(), name='cliente-detail'),
 ]
